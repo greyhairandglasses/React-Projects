46781dee5b012423d375a2d0119755f6916b1423
Tom <tomarcher88@outlook.com>
2020-09-08 08:36:19 +0100

    Initial upload of build

---
+++ weather-app/build/precache-manifest.db084c22530100d4b527e05ef8d87d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90866741862cda53674d48e392a82289",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a853286278ff91e21f",
+    "url": "/static/css/main.1b1d0569.chunk.css"
+  },
+  {
+    "revision": "19b9aa2f60262c6d9691",
+    "url": "/static/js/2.1665b52e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1665b52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a853286278ff91e21f",
+    "url": "/static/js/main.7735a767.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
